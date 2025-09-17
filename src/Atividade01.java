1a850e0e05f556a8ae9d0474cdd54cfd8f4a91b6
Matheus Reichemback Stang <matheus.reichemback.stang@gmail.com>
2025-09-17 15:06:28 -0300

    refactor: explicando partes do código e melhorando sua funcionalidade

--- src/Atividade01.java
+++ src/Atividade01.java
@@ -3,29 +3,35 @@ import java.util.Scanner;
 public class Atividade01 {
 
     public static void main(String[] args) {
+        // Definindo variáveis a serem usadas para armazenar valores e ler valores
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
 
 
-        System.out.println("Informe 10 números inteiros");
+        // Recolhendo os números definindos pelo usuário
+        System.out.println("#######################################");
+        System.out.println("INFORME 10 NÚMEROS INTEIROS:");
+        System.out.println("#######################################");
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Qual vai ser o " + (i + 1) + "° número?");
             numeros[i] = scanner.nextInt();
         }
 
-        int maior = 0;
+        // Variáveis que armazenam o maior e o menor valor
+        int maior = numeros[0];
+        int menor = numeros[0];
+        // Verificando e atribuindo o maior e o menor valor digitado pelo usuário
         for (int i = 0; i < numeros.length; i++) {
             if(numeros[i] > maior){
                 maior = numeros[i];
             }
-        }
-
-        int menor = numeros[0];
-        for (int i = 0; i < numeros.length; i++) {
             if(numeros[i] < menor){
                 menor = numeros[i];
             }
         }
+
+        // Imprimindo na tela o resultado
+        System.out.println("#######################################");
         System.out.println("O maior valor digitado é: " + maior);
         System.out.println("O menor valor digitado é: " + menor);
 
--- src/Atividade02.java
+++ src/Atividade02.java
@@ -3,13 +3,16 @@ import java.util.Random;
 public class Atividade02 {
 
     public static void main(String[] args) {
+        // Definindo variáveis que vão ser usadas
         Random random = new Random();
         int[] numeros = new int[15];
 
+        // Atribuindo números aleatórios no array "numeros"
         for(int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt((100 - 1) + 1) + 1;
+            numeros[i] = random.nextInt(100) + 1;
         }
 
+        // Definindo o tipo (par/ímpar) de cada número do array "numeros" e imprimindo na tela
         for(int i = 0; i < numeros.length; i++) {
             int valor = numeros[i] % 2;
             if(valor == 0) {
